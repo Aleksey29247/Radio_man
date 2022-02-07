4a18c931f45a92272f3a3743d8d2867003671e48
Aleksey <Alik29247@gmail.com>
2022-02-07 20:16:08 +0700

    bad hist

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -4,32 +4,15 @@ public class Radio {
     //поля
     private int currentvolume = 0;
     private int ch = 0;
-    private  int chmax;
-    // придел по звуку
-    private  int volumemax;
 
-    public Radio() {
-        this.chmax=10-1;
-        this.volumemax=10;
-    }
 
-    public Radio(int cmax){
-        this.chmax=cmax-1;
-        this.volumemax=100;
-    }
-
-    public Radio(int cmax, int volmax)
-    {
-        this.chmax=cmax-1;
-        this.volumemax=volmax;
-    }
     //увеличение громкости
     public void increasevolume() {
 
-        if (currentvolume < volumemax) {
+        if (currentvolume < 10) {
             currentvolume++;
         } else {
-            currentvolume = volumemax;
+            currentvolume = 10;
         }
     }
 
@@ -47,7 +30,7 @@ public class Radio {
     public void next() {
 
         ch++;
-        if (ch > this.chmax) {
+        if (ch > 9) {
             ch = 0;
         }
     }
@@ -56,7 +39,7 @@ public class Radio {
     public void prev() {
         ch--;
         if (ch < 0) {
-            ch = this.chmax;
+            ch = 9;
         }
 
     }
@@ -64,7 +47,7 @@ public class Radio {
     //установить канал
     public void setch(int ch) {
         if (ch >= 0) {
-            if (ch <= this.chmax) {
+            if (ch <=9) {
                 this.ch = ch;
             } else {
                 this.ch = 0;
@@ -77,21 +60,14 @@ public class Radio {
     public int getch() {
         return this.ch;
     }
- public  int getChmax()
- {
-     return this.chmax;
- }
- public void setChmax(int cmax)
- {
-   this.chmax=cmax;
- }
+
     public int getcurrentvolume() {
         return currentvolume;
     }
 
     public void setcurrentvolume(int currentvolume) {
         if (currentvolume >= 0) {
-            if (currentvolume <= volumemax) {
+            if (currentvolume <= 10) {
                 this.currentvolume = currentvolume;
             } else {
                 this.currentvolume = 0;
@@ -99,6 +75,7 @@ public class Radio {
         } else {
             this.currentvolume = 0;
         }
+
 
     }
 }
